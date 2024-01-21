a13e39f5993e534816797f4482dd8c37d6014e42
dushyant-2002 <dushyant.workspace@gmail.com>
2024-01-21 23:39:54 +0530

    Counter Project using useState built in hook

---
+++ lec3-Project-Counter/02-Counter_Project/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let Counter = 5;
+  // const increaseValue = ()=>{
+  //   console.log("clicked",Math.random());
+  //   Counter++;//here value is increased but did'not get rendered in our webpage for rendering we need hooks
+  // }
+  //using hooks
+  const [counter,setCounter] = useState(0);
+  const  increaseValue = ()=>{
+    if(counter == 20){
+      return;
+    }
+    setCounter(counter+1);
+    console.log("counter increased in ui also")
+  }
+  const  decreaseValue = ()=>{
+    if(counter == 0){
+      return;
+    }
+    setCounter(counter-1);
+    console.log("counter decreased in ui also")
+  }
+
+
+  return (
+    <>
+      <h1>Chai Aur Counter</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={increaseValue}>increase Value {counter}</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease Value {counter} </button>
+    </>
+  )
+}
+
+export default App
